a3afec8ec20802a1ded68822e5f19e0dbeef372a
MD XF <rkcoder2002@gmail.com>
2017-10-17 09:13:35 -0700

    Delete fork-bomb.asm

--- fork-bomb.asm
+++
@@ -1,8 +0,0 @@
-section .text
- global _start ;Call start
- 
-_start:
- push byte 2      ;syscall to Linux fork
- pop eax          ;set EAX argument for fork to NULL [So it works in strings]
- int 0x80         ;Execute syscall with fork & the EAX [null, above] argument
- jmp short _start ;Go back to beginning, causing a fork bomb
